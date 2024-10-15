168600fc392e6160c8a28f2d92ba75f9d745dd95
Mari <mariannasantiagogodoy58@gmail.com>
2024-10-15 11:51:47 -0300

    onsizealocated

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -2,12 +2,16 @@
 
 public partial class MainPage : ContentPage
 {
-	const int gravidade=5;
-	const int TempoEntreFrames=25;
-	bool EstaMorto=true;
-	double LarguraJanela=0;
-	double AlturaJanela=0;
-	int Velocidade=20;
+	const int gravidade = 5;
+	const int TempoEntreFrames = 25;
+	bool EstaMorto = true;
+	double LarguraJanela = 0;
+	double AlturaJanela = 0;
+	int Velocidade = 20;
+	const int ForcaPulo = 60;
+	bool EstaPulando = false;
+	int TempoPulando = 0;
+	const int maxTempoPulando = 3;
 
 
 	public MainPage()
@@ -24,43 +28,102 @@ public partial class MainPage : ContentPage
 	async Task Desenhar()
 	{
 		while (!EstaMorto)
+
 		{
-			AplicaGravidade();
+			if (EstaPulando)
+				AplicaPulo();
+			else
+				AplicaGravidade();
 			await Task.Delay(TempoEntreFrames);
 			GerenciaCanos();
+			if (VerificaColisao())
+			{
+				EstaMorto = true;
+				FrameGameOver.IsVisible = true;
+				break;
+			}
+			await Task.Delay(TempoEntreFrames);
 		}
 	}
 
-    
+
 	void OnGameOverClicked(object s, TappedEventArgs a)
 	{
-		FrameGameOver.IsVisible=false;
+		FrameGameOver.IsVisible = false;
 		Inicializar();
 		Desenhar();
 	}
 
 	void Inicializar()
 	{
-		EstaMorto=false;
-		ImagePassarinho.TranslationY=0;
+		EstaMorto = false;
+		ImagePassarinho.TranslationY = 0;
 	}
 
-    protected override void OnSizeAllocated(double width, double height)
-    {
-        base.OnSizeAllocated(width, height);
-		LarguraJanela=width;
-		AlturaJanela=height;
-    }
+	protected override void OnSizeAllocated(double width, double height)
+	{
+		base.OnSizeAllocated(width, height);
+		LarguraJanela = width;
+		AlturaJanela = height;
+	}
 
 	void GerenciaCanos()
 	{
-		CanoCima.TranslationX-=Velocidade;
-		CanoBaixo.TranslationX-=Velocidade;
-		if(CanoBaixo.TranslationX<=-LarguraJanela)
+		CanoCima.TranslationX -= Velocidade;
+		CanoBaixo.TranslationX -= Velocidade;
+		if (CanoBaixo.TranslationX <= -LarguraJanela)
 		{
-		CanoCima.TranslationX=0;
-		CanoBaixo.TranslationX=0;
+			CanoCima.TranslationX = 0;
+			CanoBaixo.TranslationX = 0;
 		}
 	}
+
+	bool VerificaColisaoTeto()
+	{
+		var minY = -AlturaJanela / 2;
+		if (ImagePassarinho.TranslationY <= minY)
+			return true;
+		else
+			return false;
+	}
+
+	bool VerificaColisaoChao()
+	{
+		var maxY=AlturaJanela/ 2 -Chao.HeightRequest;
+		if (ImagePassarinho.TranslationY <= maxY)
+			return true;
+		else
+			return false;
+	}
+
+	bool VerificaColisao()
+	{
+		if (!EstaMorto)
+		{
+			if (VerificaColisaoTeto() ||
+			VerificaColisaoChao())
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void AplicaPulo()
+	{
+		ImagePassarinho.TranslationY -= ForcaPulo;
+		TempoPulando++;
+		if (TempoPulando >= maxTempoPulando)
+		{
+			EstaPulando = false;
+			TempoPulando = 0;
+		}
+	}
+
+	void OnGridClicked(object s, TappedEventArgs a)
+	{
+		EstaPulando = true;
+	}
+
 }
 
